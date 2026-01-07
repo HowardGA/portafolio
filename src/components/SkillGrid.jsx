11fde01264e25e8d244e29c03433b229bd1b31e4
“Howard <higamelsa430@gmail.com>
2026-01-06 23:46:53 -0800

    Update skill percentages in SkillsGrid component

--- src/components/SkillGrid.jsx
+++ src/components/SkillGrid.jsx
@@ -4,19 +4,19 @@ import { SiNextdotjs, SiDjango, SiMysql, SiPostgresql, SiTypescript } from "reac
 import { SiTailwindcss } from 'react-icons/si';
 import { SkillCard } from './SkillCard';
 const skills = [
-  { icon: <FaReact />, name: 'React', percentage: 65 },
+  { icon: <FaReact />, name: 'React', percentage: 70 },
   { icon: <FaHtml5 />, name: 'HTML5', percentage: 90 },
-  { icon: <FaCss3Alt />, name: 'CSS3', percentage: 85 },
-  { icon: <FaJsSquare />, name: 'JavaScript', percentage: 70 },
-  { icon: <SiTailwindcss />, name: 'Tailwind CSS', percentage: 80 },
-  { icon: <FaNodeJs />, name: 'Node.js', percentage: 55 },
-  { icon: <FaPython />, name: 'Python', percentage: 75 },
+  { icon: <FaCss3Alt />, name: 'CSS3', percentage: 70 },
+  { icon: <FaJsSquare />, name: 'JavaScript', percentage: 80 },
+  { icon: <SiTailwindcss />, name: 'Tailwind CSS', percentage: 70 },
+  { icon: <FaNodeJs />, name: 'Node.js', percentage: 70 },
+  { icon: <FaPython />, name: 'Python', percentage: 55 },
   { icon: <SiNextdotjs />, name: 'NextJS', percentage: 65 },
   { icon: <FaLaravel />, name: 'Laravel REST', percentage: 65 },
   { icon: <SiDjango />, name: 'Django REST', percentage: 65 },
-  { icon: <SiMysql />, name: 'MySQL', percentage: 75 },
+  { icon: <SiMysql />, name: 'MySQL', percentage: 80 },
   { icon: <SiPostgresql />, name: 'PostgreSQL', percentage: 75 },
-  { icon: <SiTypescript />, name: 'TypeScript', percentage: 75 },
+  { icon: <SiTypescript />, name: 'TypeScript', percentage: 70 },
   { icon: <FaGitAlt />, name: 'Git', percentage: 65 },
   { icon: <FaWifi />, name: 'IoT', percentage: 65 },
   
